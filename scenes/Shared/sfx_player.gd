46ad902014ff84c37d2e04bee5ce752abe173516
zriL <cyril.savary.42@gmail.com>
2026-07-05 18:52:41 +0200

    pyromane probleme sfx

--- scenes/Shared/sfx_player.gd
+++ scenes/Shared/sfx_player.gd
@@ -1,9 +1,10 @@
 extends Node
 
-func play(sound: AudioStream) -> void :
+func play(sound: AudioStream, volume: float = 0) -> void :
 	for child in get_children() :
 		var player = child as AudioStreamPlayer
 		if !player.playing :
 			player.stream = sound
+			player.volume_db = volume
 			player.play()
 			break
