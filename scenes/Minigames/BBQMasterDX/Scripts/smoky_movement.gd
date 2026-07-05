8fdac22f03ace75ffb242e3741c28f5441f877b5
SushiDa <ctrn42@gmail.com>
2026-07-05 16:59:58 +0200

    SFX Musique MURICA

--- scenes/Minigames/BBQMasterDX/Scripts/smoky_movement.gd
+++ scenes/Minigames/BBQMasterDX/Scripts/smoky_movement.gd
@@ -6,6 +6,10 @@ class_name BBQMaster_Smokey
 @export var bbq_min_distance: float
 @export var player_min_distance: float
 
+@export var music_delay: float
+@export var sfx_wake_up: AudioStream
+@export var wake_up_music: AudioStream
+
 enum SmokeyStatus { SLEEP, AWAKE, PRE_ATTACK, ATTACKING }
 
 var target_bbq: BBQMaster_BBQ
@@ -13,9 +17,12 @@ var status: SmokeyStatus = SmokeyStatus.SLEEP
 
 var locked_bbq: BBQMaster_BBQ
 
+var wake_up_timer: Timer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	wake_up_timer = Timer.new()
+	add_child(wake_up_timer)
+	wake_up_timer.timeout.connect(play_wake_up_music)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,8 +50,16 @@ func _process(delta: float) -> void:
 			status = SmokeyStatus.AWAKE
 
 func wake_up():
-	status = SmokeyStatus.AWAKE
+	MusicPlayer.stop();
+	SFXPlayer.play(sfx_wake_up)
+	wake_up_timer.wait_time = music_delay
+	wake_up_timer.start()
 	pass
 
 func is_awake() -> bool:
 	return status != SmokeyStatus.ATTACKING
+
+func play_wake_up_music():
+	wake_up_timer.stop()
+	status = SmokeyStatus.AWAKE
+	MusicPlayer.play(wake_up_music)
